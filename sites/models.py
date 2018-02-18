82125d39ba4d7576c8405d2bba6cc9b0726afb92
Bruno Furtado <brunofurtadoc@gmail.com>
2018-02-18 18:36:06 -0300

    Add site model

--- sites/models.py
+++ sites/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Site(models.Model):
+    name = models.CharField(max_length=255)
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
